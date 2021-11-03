d7c082e0493f6e62f165d63214b0de4628708a5c
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-11-03 10:51:29 -0500

    create the slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://ae01.alicdn.com/kf/HTB1tQXVa9_I8KJjy0Foq6yFnVXaT/Coshome-One-piece-Monkey-D-Luffy-Conjunto-de-ropa-de-verano-para-Halloween-disfraz-camisa-pantalones.jpg',
+        title: 'SUMMER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd',
+    },
+    {
+        id: 2,
+        img: 'https://litb-cgis.rightinthebox.com/images/640x640/201908/xws1565342372110.jpg',
+        title: 'WINTER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: 'https://i.pinimg.com/originals/09/33/a7/0933a7b7d5d2bf10b5f72031a0671eef.jpg',
+        title: 'POPULAR SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fbf0f4',
+    }
+]
