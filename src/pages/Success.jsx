52a02ff47297d9833b6575bac7fead7c066b3fc0
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-02-08 21:31:08 -0500

    request to order in success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { userRequest } from '../requestMethods';
 
 export const Success = () => {
 
     const location = useLocation();
     const data = location.state.stripeData;
-    const cart = location.state.cart;
-
+    const cart = location.state.products;
+    const currentUser = useSelector(state => state.user.currentUser);
     const [orderId, setOrderId] = useState(null);
+    const navigate = useNavigate();
 
-    console.log(location);
+    useEffect(() => {
+        const createOrder = async () => {
+            try {
+                const res = await userRequest.post("/orders", {
+                    userId: currentUser._id,
+                    products: cart.products.map((item) => ({
+                        productId: item._id,
+                        quantity: item._quantity,
+                    })),
+                    amount: cart.total,
+                    address: data.billing_details.address,
+                });
+                setOrderId(res.data._id);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        data && createOrder();
+    }, [cart, data, currentUser]);
+
+
+    const onClickHome = () => {
+        navigate('/');
+    }
+
     return (
         <div
             style={{
@@ -23,7 +50,7 @@ export const Success = () => {
             {orderId
                 ? `Order has been created successfully. Your order number is ${orderId}`
                 : `Successfull. Your order is being prepared...`}
-            <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+            <button style={{ padding: 10, marginTop: 20 }} onClick={onClickHome}>Go to Homepage</button>
         </div>
     )
 }
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { API } from './config';
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZGNiZmRjY2MyMWFjZmQ3OGNmMWZiMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MjEyOTIxNywiZXhwIjoxNjQyMjE1NjE3fQ.0DP70csQZzfAaUztWncCSg2per7Nd_Sq7RCPjV5j8uc';
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.token;
 
 export const publicRequest = axios.create({
     baseURL: API,
@@ -9,5 +11,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: API,
-    header: { token: `Bearer ${TOKEN}` },
+    headers: { token: `Bearer ${TOKEN}` },
 });
